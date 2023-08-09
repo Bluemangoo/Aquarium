741bce85eadca20c5924ef8d02a6861dc628a5bf
bluemangoo <chenfy2006@qq.com>
2023-08-09 19:54:54 +0800

    Fix (Backend): 修正不走缓存的问题

--- src/utils/checkPrebuildFile.ts
+++ src/utils/checkPrebuildFile.ts
@@ -2,8 +2,8 @@ import * as fs from "fs";
 
 export default function checkPrebuildFileOr(file: string, or: () => any) {
     try {
-        fs.accessSync(process.cwd() + file, fs.constants.F_OK);
-        return fs.readFileSync(process.cwd() + file).toString();
+        fs.accessSync(process.cwd() + "/" + file, fs.constants.F_OK);
+        return fs.readFileSync(process.cwd() + "/" + file).toString();
     } catch (err) {
         return or();
     }
