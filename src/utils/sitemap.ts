1e228df36c1ace9c98606b3e0f6751bbf8ee6f06
Bluemangoo <chenfy2006@qq.com>
2024-03-24 12:44:47 +0800

    Fix (UI): 修复 sitemap

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -1,12 +1,11 @@
 import bucket from "../software/bucket";
 import getLastModify from "./getLastModify";
-import { execSync } from "node:child_process";
 
-const urlBase = "https://aquarium.bluemnangoo.net";
+const urlBase = "https://aquarium.bluemangoo.net";
 
 export default function sitemap() {
     let xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-        "<urlset xmlns=\"https://www.sitemaps.org/schemas/sitemap/0.9\">";
+        "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">";
     // index
     xml += `<url><loc>${urlBase}</loc><priority>0.8</priority></url>`;
 
